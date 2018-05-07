d38f9ee4e344b9c7c3a9a19e11204e69a92f62e7
CatPerry <27353719+CatPerry@users.noreply.github.com>
2018-05-07 18:50:26 -0400

    removed reference to sqlite3

--- models.rb
+++ models.rb
@@ -1,11 +1,6 @@
 require "active_record"
 require "sqlite3"
 
-ActiveRecord::Base.establish_connection(
-  adapter:  "sqlite3",
-  database: "app.db"
-)
-
 class User < ActiveRecord::Base
   has_one :profile
   has_many :posts
